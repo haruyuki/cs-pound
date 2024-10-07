612a63f0dab956fd4c1bd2acd06b70572e787c9b
Oliver Lin <jumpy12359@gmail.com>
2024-10-08 01:56:53 +1100

    :sparkles: Add remindme command

---
+++ commands/general/remindme.js
@@ -0,0 +1,66 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+function formatter([hour, minute, second]) {
+    return [
+        hour ? `${hour} hour${hour > 1 ? "s" : ""}${minute || second ? ", " : ""}` : "",
+        minute ? `${minute} minute${minute > 1 ? "s" : ""}${second ? " and " : ""}` : "",
+        second ? `${(hour || minute) ? "and " : ""}${second} second${second > 1 ? "s" : ""}` : ""
+    ].join('');
+}
+
+function parseTimeString(amount) {
+    const times = { h: 0, m: 0, s: 0 };
+
+    if (/^\d+$/.test(amount)) {
+        times.m = parseInt(amount, 10);
+    } else {
+        if (!/^\d+[hms](\d+[hms])*$/.test(amount)) {
+            return [0, 0, 0];
+        }
+
+        const matches = amount.match(/(\d+)([hms])/g) || [];
+
+        matches.forEach(match => {
+            const value = parseInt(match.slice(0, -1), 10);
+            const unit = match.slice(-1);
+            times[unit] = value;
+        });
+    }
+
+    if (times.s >= 60) {
+        times.m += Math.floor(times.s / 60);
+        times.s = times.s % 60;
+    }
+
+    if (times.m >= 60) {
+        times.h += Math.floor(times.m / 60);
+        times.m = times.m % 60;
+    }
+
+    return [times.h, times.m, times.s];
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('remindme')
+        .setDescription('Pings you after a specified amount of time.')
+        .addStringOption(option =>
+            option.setName('time')
+                .setDescription('The amount of time to wait before reminding you (e.g., 10s, 5m, 1h, 1h5m10s)')
+                .setRequired(true)),
+    async execute(interaction) {
+        const amount = interaction.options.getString('time');
+        const data = parseTimeString(amount);
+        const milliseconds = (data[0] * 60 * 60 + data[1] * 60 + data[2]) * 1000;
+        if (data.every(value => value === 0)) {
+            return await interaction.reply({ content: "Invalid time format. Please provide a valid time format (e.g., 10s, 5m, 1h, 1h5m10s).", ephemeral: true });
+        }
+
+        const total = formatter(data);
+        await interaction.reply({ content: `A reminder has been set for you in ${total}.`, ephemeral: true });
+
+        setTimeout(() => {
+            interaction.followUp({ content: `${interaction.user}, this is your ${amount} reminder!` });
+        }, milliseconds);
+    },
+};
