be1281c4dbc629753203e9743a0138c6fb8d1abb
Oliver Lin <jumpy12359@gmail.com>
2024-10-10 14:20:12 +1100

    :art: Optimise deploy-commands for ESM

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,40 +1,49 @@
-const { REST, Routes } = require("discord.js")
-const { clientID, guildID, token } = require("./config.json")
-const fs = require("node:fs")
-const path = require("node:path")
+import { readdirSync } from "node:fs"
+import { dirname, join } from "node:path"
+import { fileURLToPath, pathToFileURL } from "node:url"
+import { REST, Routes } from "discord.js"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const commands = []
-const foldersPath = path.join(__dirname, "commands")
-const commandFolders = fs.readdirSync(foldersPath)
+const foldersPath = join(__dirname, "commands")
+const commandFolders = readdirSync(foldersPath)
 
-for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder)
-    const commandFiles = fs
-        .readdirSync(commandsPath)
-        .filter((file) => file.endsWith(".js"))
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file)
-        const command = require(filePath)
-        if ("data" in command && "execute" in command) {
-            commands.push(command.data.toJSON())
-        } else {
-            console.log(
-                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-            )
+async function loadCommands() {
+    for (const folder of commandFolders) {
+        const commandsPath = join(foldersPath, folder)
+        const commandFiles = readdirSync(commandsPath).filter((file) =>
+            file.endsWith(".js"),
+        )
+        for (const file of commandFiles) {
+            const filePath = pathToFileURL(join(commandsPath, file))
+            const command = await import(filePath)
+            if ("data" in command && "execute" in command) {
+                commands.push(command.data.toJSON())
+            } else {
+                console.log(
+                    `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+                )
+            }
         }
     }
-}
 
-const rest = new REST().setToken(token)
+    const rest = new REST().setToken(process.env.DISCORD_TOKEN)
 
-;(async () => {
     try {
         console.log(
             `Started refreshing ${commands.length} application (/) commands.`,
         )
 
         const data = await rest.put(
-            Routes.applicationGuildCommands(clientID, guildID),
+            Routes.applicationGuildCommands(
+                process.env.CLIENT_ID,
+                process.env.GUILD_ID,
+            ),
             // Routes.applicationCommands(clientID),
             { body: commands },
         )
@@ -45,7 +54,9 @@ const rest = new REST().setToken(token)
     } catch (error) {
         console.error(error)
     }
-})()
+}
+
+loadCommands().then(() => console.log("Commands deployed!"))
 
 // for guild-based commands
 // rest.delete(Routes.applicationGuildCommand(clientID, guildID, commandID))
