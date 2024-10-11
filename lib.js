e99aa1afa93f1cfc7936f934c9c453f91e974c9a
Oliver Lin <jumpy12359@gmail.com>
2024-10-11 20:38:59 +1100

    :art: Add headers when sending requests

--- lib.js
+++ lib.js
@@ -1,6 +1,12 @@
 import { launch } from "rebrowser-puppeteer"
 import Sequelize, { NUMBER, STRING } from "sequelize"
 
+export const BOT_VERSION = "2024.10.09"
+export const HEADERS = {
+    "User-Agent": "CS Pound Discord Bot Agent " + BOT_VERSION,
+    "From": "haru@haruyuki.moe",
+}
+
 export const sequelize = new Sequelize({
     dialect: "sqlite",
     logging: false,
@@ -51,6 +57,7 @@ export const ItemDB = sequelize.define(
 export const getPoundTime = async () => {
     const browser = await launch({ userDataDir: "./chrome_data" })
     const page = await browser.newPage()
+    await page.setExtraHTTPHeaders(HEADERS);
     await page.setRequestInterception(true)
     page.on("request", (request) => {
         if (request.resourceType() === "image") request.abort()
